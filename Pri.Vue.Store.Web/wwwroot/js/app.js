495f331ef34834655220c70973558a9387fbbb52
Miledi75 <mileto.di.marco@howest.be>
2023-12-01 14:32:43 +0100

    Add category create

---
+++ Pri.Vue.Store.Web/wwwroot/js/app.js
@@ -0,0 +1,105 @@
+﻿
+    var app = new Vue({
+        el: "#app",
+    data: {
+        showCategoriesSection: false,
+    showProductsSection: false,
+    showProductDetailSection: false,
+    showErrorSection: false,
+    categories: null,
+    products: null,
+    categorySearch: "",
+    productSearch: "",
+    productDetail: null,
+    errorMessage: "",
+    isLoading: false
+            },
+    methods: {
+        showProductsFromCategory: async function(categoryId){
+        this.isLoading = true;
+    await axios.get(`${baseUrl}categories/${categoryId}/products`)
+                        .then((response) => {
+        this.products = response.data;
+    this.showCategoriesSection = false;
+    this.showProductsSection = true;
+    this.showProductDetailSection = false;
+                        })
+                        .catch((e) => {
+        this.showErrorSection = true;
+    this.errorMessage = e.message
+                        })
+                        .finally(() => {
+        this.isLoading = false;
+                        });
+                },
+    showProductDetails: async function (product) {
+        this.isLoading = true;
+    await axios.get(`${baseUrl}products/${product.id}`)
+                        .then((response) => {
+        this.productDetail = response.data;
+    this.showCategoriesSection = false;
+    this.showProductsSection = false;
+    this.showProductDetailSection = true;
+                        })
+                        .catch((e) => {
+        this.showErrorSection = true;
+    this.errorMessage = e.message
+                        })
+                        .finally(() => {
+        this.isLoading = false;
+                        });
+                },
+    searchCategory: async function () {
+        await axios.get(`${baseUrl}categories?search=${this.categorySearch}`)
+            .then((response) => {
+                this.categories = response.data
+            })
+            .catch((e) => {
+                this.showErrorSection = true;
+                this.errorMessage = e.message
+            });
+                },
+    searchProduct: async function () {
+                    const productsFromApi = await axios.get(`${baseUrl}products?search=${this.productSearch}`)
+                        .then((response) => {
+        this.products = response.data
+    })
+                        .catch((e) => {
+        this.showErrorSection = true;
+    this.errorMessage = e.message
+                        });
+                },
+    showCategories: async function () {
+        await axios.get(`${baseUrl}categories`)
+            .then((response) => {
+                this.categories = response.data
+            })
+            .catch((e) => {
+                this.showErrorSection = true;
+                this.errorMessage = e.message
+            })
+            .finally(() => {
+                this.isLoading = false;
+            });
+    this.showCategoriesSection = true;
+    this.showProductsSection = false;
+    this.showProductDetailSection = false;
+                },
+    showProducts: async function () {
+        await axios.get(`${baseUrl}products`)
+            .then((response) => {
+                this.products = response.data
+            })
+            .catch((e) => {
+                this.showErrorSection = true;
+                this.errorMessage = e.message
+            })
+            .finally(() => {
+                this.isLoading = false;
+            });
+    this.showCategoriesSection = false;
+    this.showProductsSection = true;
+    this.showProductDetailSection = false;
+                },
+            }
+        });
---
+++ Pri.Vue.Store.Web/wwwroot/js/categoriesCrud.js
@@ -0,0 +1,114 @@
+﻿let vue = new Vue({
+    el: "#app",
+    name: "categoriesAdminSection",
+    data: {
+        showCategoriesSection: false,
+        showErrorSection: false,
+        categories: null,
+        errorMessage: "",
+        isLoading: false,
+        categoryError: false,
+        baseUrl: "https://localhost:7086/api",
+        newCategoryModel: {
+            name: ""
+        },
+        selectedCategory: {
+            id: "",
+            name: "",
+            imageUrl: "",
+        }
+    },
+    created: async function () {
+        // pas dit aan zodat enkel admins de tabel met categorieën kunnen zien
+        // gebruikers die geen admin zijn zien onderstaande melding:
+        this.errorMessage = "Enkel admins kunnen categorieën aanpassen. Je hebt onvoldoende rechten.";
+        this.showErrorSection = false;
+        //call showCategories
+        this.showCategories();
+    },
+    methods: {
+        showCategories: async function () {
+            this.isLoading = true;
+            const url = `${this.baseUrl}/categories`;
+            this.categories =
+                await axios.get(url)
+                    .then(response => response.data)
+                    .catch(error => {
+                        this.error = true;
+                        this.errorMessage = error.message;
+                    })
+                    .finally(() => {
+                        this.isLoading = false;
+                    });
+            if (this.categories !== undefined) {
+                this.showCategoriesSection = true;
+            }
+            // Voorzie de nodige code om categorieën op te vragen van de API
+            // Denk aan de logica omtrent het al of niet tonen van de errorSection, categoriesSection, ...
+            // Bekijk hiervoor de aanwezige properties in de data property
+        },
+        createCategory: async function () {
+            if (this.newCategoryModel.name.trim() === "") {
+                this.categoryError = true;
+                this.errorMessage = "please provide a category";
+                return;
+            }
+            this.isLoading = true;
+            //call the api
+            const url = `${this.baseUrl}/categories`;
+            const token = sessionStorage.getItem('token');
+            const headers = {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    }
+            };
+            await axios.post(url, this.newCategoryModel,headers)
+                .then(response => response.data)
+                .catch(error => {
+                    this.error = true;
+                    this.errorMessage = error.message;
+                })
+                .finally(() => {
+                    this.isLoading = false;
+                });
+            //load the categories
+            this.showCategories();
+            //close the modal
+            $('#createCategoryModal').modal('hide');
+            this.newCategoryModel.name = "";
+            // Voorzie de nodige code om categorieën aan te maken via de API
+            // Denk aan de logica omtrent het al of niet tonen van de errorSection, categoriesSection, ...
+            // Bekijk hiervoor de aanwezige properties in de data property
+        },
+        updateCategory: async function (categoryToUpdate) {
+            // Voorzie de nodige code om bestaande categorieën aan te passen via de API
+            // Denk aan de logica omtrent het al of niet tonen van de errorSection, categoriesSection, ...
+            // Bekijk hiervoor de aanwezige properties in de data property
+        },
+        deleteCategory: async function (categoryToDelete) {
+            // Voorzie de nodige code om bestaande categorieën aan te verwijderen via de API
+            // Denk aan de logica omtrent het al of niet tonen van de errorSection, categoriesSection, ...
+            // Bekijk hiervoor de aanwezige properties in de data property
+        },
+        showUpdateModal: function (category) {
+            this.selectedCategory = category;
+            const modalElement = document.getElementById("updateCategoryModal");
+            const modal = bootstrap.Modal.getInstance(modalElement);
+            modal.show();
+        },
+        showCreateModal: function () {
+            this.categoryError = false;
+            $('#createCategoryModal').modal('show');
+        },
+        hideCreateModal: function () {
+            $('#createCategoryModal').modal('hide');
+        },
+        showDeleteModal: function (category) {
+            // Werk deze methode zelf uit
+        },
+        cancelCategoryAction: function () {
+            // reset newCategoryModel
+            // reset selectedCategory
+        }
+    }
+});
